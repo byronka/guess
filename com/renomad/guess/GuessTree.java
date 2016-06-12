fd88efdabff7f4a29bfe5ea301a0c1b4c003e889
Byron Katz <byronka@msn.com>
2016-06-12 13:42:29 -0400

    initial foray into building binary search tree

---
+++ com/renomad/guess/GuessTree.java
@@ -0,0 +1,19 @@
+/**
+  * A standard binary tree, to be used in finding
+  * the user's number choice.
+  * we'll make these nodes immutable to avoid needing
+  * getters / setters.
+  */
+public class GuessTreeNode {
+
+  public GuessTreeNode(int value) {
+
+  }
+
+  public final int value;
+  public final GuessTreeNode left;
+  public final GuessTreeNode right;
+
+  
+
+}
