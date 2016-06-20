254c9126d995942ece5029e0b8940cf799cce5b0
Byron Katz <bkatz@costar.com>
2016-06-20 16:47:32 -0400

    argh annoying.  some error while running tests. test:     [junit] Running com.renomad.guess.GuessTests     [junit] Testsuite: com.renomad.guess.GuessTests     [junit] Tests run: 1, Failures: 0, Errors: 1, Skipped: 0, Time elapsed: 0 sec     [junit] Tests run: 1, Failures: 0, Errors: 1, Skipped: 0, Time elapsed: 0 sec     [junit]     [junit]     Caused an ERROR     [junit] com.renomad.guess.GuessTests     [junit] java.lang.ClassNotFoundException: com.renomad.guess.GuessTests     [junit]     at java.net.URLClassLoader.findClass(URLClassLoader.java:381)     [junit]     at java.lang.ClassLoader.loadClass(ClassLoader.java:424)     [junit]     at sun.misc.Launcher$AppClassLoader.loadClass(Launcher.java:331)     [junit]     at java.lang.ClassLoader.loadClass(ClassLoader.java:357)     [junit]     at java.lang.Class.forName0(Native Method)     [junit]     at java.lang.Class.forName(Class.java:264)     [junit]

---
+++ src/com/renomad/guess/GuessTests.java
@@ -0,0 +1,21 @@
+package com.renomad.guess;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import com.renomad.guess.Guess;
+
+public class GuessTests {
+
+
+  @Test
+  public void parametric_test_of_calcs() {
+    assertTrue(0 == 1);
+  }
+
+}
